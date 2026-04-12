efd2957fbea56bb323d4f31654635013fc67b1eb
IOSA33 <sedinkin33@gmail.com>
2026-04-12 20:00:15 +0300

    jpg reader/writer

--- main.cpp
+++ main.cpp
@@ -1,15 +1,33 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "libs/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "libs/stb_image_write.h"
 #include <iostream>
-#include <fstream>
-#include <iterator>
-#include <vector>
+#include <chrono>
 
 int main() {
-    std::ifstream input("../photos/sample1.jpg", std::ios::binary );
-    if (!input.is_open()) {
-        std::cout << "Cannot open a photo!" << std::endl;
-        return 0;
+    const auto start { std::chrono::high_resolution_clock::now() };
+    int width, height, channels;
+    unsigned char* data { stbi_load("../photos/sample1.jpg", &width, &height, &channels, 0) };
+    if (!data) {
+        std::cout << "Failed to load INPUT image\n";
+        return 1;
     }
 
-    std::vector<unsigned char> buffer(std::istreambuf_iterator<char>(input), {});
-    std::cout << "Buffer is about " << buffer.size()/1048576 << " MB\n";
+    for (int i{ 0 }; i < width * height; ++i) {
+        int index { i * channels };
+
+        data[index + 0] = 255;
+        data[index + 1] = 0;
+        data[index + 2] = 0;
+    }
+
+    if (!stbi_write_jpg("../photos/sample1_copy.jpg", width, height, channels, data, 100)) {
+        std::cout << "Failed to write OUTPUT image\n";
+    }
+
+    stbi_image_free(data);
+
+    const auto end { std::chrono::high_resolution_clock::now() };
+    std::cout << "Time used: " << std::chrono::duration<double>(end - start) << std::endl;
 }
