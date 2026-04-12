9ddf86752c63050805dab15479c0d6a5ab653e0f
IOSA33 <sedinkin33@gmail.com>
2026-04-12 18:54:18 +0300

    photo buffer

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <iterator>
+#include <vector>
 
 int main() {
-    std::cout << "Hello GPU!" << std::endl;
+    std::ifstream input("../photos/sample1.jpg", std::ios::binary );
+    if (!input.is_open()) {
+        std::cout << "Cannot open a photo!" << std::endl;
+        return 0;
+    }
 
-    // TODO: 
-
-    return 0;
+    std::vector<unsigned char> buffer(std::istreambuf_iterator<char>(input), {});
+    std::cout << "Buffer is about " << buffer.size()/1048576 << " MB\n";
 }
