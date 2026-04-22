376005b01e7b9d93a63786ebd29549032b17db09
IOSA33 <sedinkin33@gmail.com>
2026-04-22 13:19:09 +0300

    hip folder

--- hip/main.hip.cpp
+++ hip/main.hip.cpp
@@ -1,62 +1,10 @@
 #define STB_IMAGE_IMPLEMENTATION
-#include "libs/stb_image.h"
+#include "../libs/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "libs/stb_image_write.h"
+#include "../libs/stb_image_write.h"
 #include <iostream>
 #include <chrono>
 #include <vector>
-
-void GaussianEdgeFilter(int width, int height, int channels, unsigned char* data) {
-    std::vector<unsigned char> source(data, data + (width * height * channels));
-
-    for (size_t y { 0 }; y < height; ++y) {
-        for (size_t x { 1 }; x < width - 1; ++x) {
-            size_t index { (y * width + x) * channels };
-            size_t prev  { (y * width + (x - 1)) * channels };
-            size_t next  { (y * width + (x + 1)) * channels };
-
-            int diffR { std::abs(source[next + 0] - source[prev + 0]) };
-            int diffG { std::abs(source[next + 1] - source[prev + 1]) };
-            int diffB { std::abs(source[next + 2] - source[prev + 2]) };
-
-            int wh{ 25 };
-
-            if ((diffR > wh && diffG > wh) || (diffR > wh && diffB > wh) || (diffG > wh && diffB > wh)) {
-                data[index + 0] = 255;
-                data[index + 1] = 255;
-                data[index + 2] = 255;
-            } else {
-                data[index + 0] = 0;
-                data[index + 1] = 0;
-                data[index + 2] = 0;
-            }
-        }
-    }
-
-    for (size_t y { 1 }; y < height - 1; ++y) {
-        for (size_t x { 0 }; x < width; ++x) {
-            size_t index { (y * width + x) * channels };
-            size_t up    { ((y - 1) * width + x) * channels };
-            size_t down  { ((y + 1) * width + x) * channels };
-
-            int diffR { std::abs(source[up + 0] - source[down + 0]) };
-            int diffG { std::abs(source[up + 1] - source[down + 1]) };
-            int diffB { std::abs(source[up + 2] - source[down + 2]) };
-
-            int wh{ 25 };
-
-            if ((diffR > wh && diffG > wh) || (diffR > wh && diffB > wh) || (diffG > wh && diffB > wh)) {
-                data[index + 0] = 255;
-                data[index + 1] = 255;
-                data[index + 2] = 255;
-            } else {
-                data[index + 0] = 0;
-                data[index + 1] = 0;
-                data[index + 2] = 0;
-            }
-        }
-    }
-}
 
 void HandVision(int width, int height, int channels, unsigned char* data) {
     unsigned char colR{ 208 };
@@ -77,7 +25,7 @@ void HandVision(int width, int height, int channels, unsigned char* data) {
         if (std::abs(data[index + 0] - colR) <= tolerance && 
             std::abs(data[index + 1] - colG) <= tolerance && 
             std::abs(data[index + 2] - colB) <= tolerance) {
-                
+            
             data[index + 0] = 255;
             data[index + 1] = 255;
             data[index + 2] = 255;
@@ -143,18 +91,21 @@ void HandVision(int width, int height, int channels, unsigned char* data) {
 int main() {
     const auto start { std::chrono::high_resolution_clock::now() };
     int width, height, channels;
-    unsigned char* data { stbi_load("../photos/3.jpg", &width, &height, &channels, 0) };
+    unsigned char* data { stbi_load("../../photos/2.jpg", &width, &height, &channels, 0) };
     if (!data) {
         std::cout << "Failed to load INPUT image\n";
         return 1;
     }
 
-    // GaussianEdgeFilter(width, height, channels, data);
     HandVision(width, height, channels, data);
 
-    if (!stbi_write_jpg("../photos/sample_output.jpg", width, height, channels, data, 100)) {
+    if (!stbi_write_jpg("../../photos/sample_output.jpg", width, height, channels, data, 100)) {
         std::cout << "Failed to write OUTPUT image\n";
     }
+
+
+
+
 
     stbi_image_free(data);
     const auto end { std::chrono::high_resolution_clock::now() };
